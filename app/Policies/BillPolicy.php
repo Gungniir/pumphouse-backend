61076ce1a704eb0ec945be2b6798e11e9e3369d2
gungniir <vladimir.kurus@yandex.ru>
2021-10-23 10:53:17 +0700

    Bill policy

--- app/Policies/BillPolicy.php
+++ app/Policies/BillPolicy.php
@@ -4,6 +4,7 @@ namespace App\Policies;
 
 use App\Models\Bill;
 use App\Models\User;
+use Illuminate\Auth\Access\Response;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class BillPolicy
@@ -11,17 +12,25 @@ class BillPolicy
     use HandlesAuthorization;
 
     /**
-     * Create a new policy instance.
+     * Perform pre-authorization checks.
      *
-     * @return void
+     * @param User $user
+     * @param string $ability
+     * @return void|bool
      */
-    public function __construct()
+    public function before(User $user, string $ability)
     {
-        //
+        if ($user->login === config('admin.login')) {
+            return true;
+        }
     }
 
     public function view(User $user, Bill $bill)
     {
+        if (is_null($user->resident)) {
+            return Response::deny("You are not a resident");
+        }
 
+        return $user->resident->id === $bill->resident_id;
     }
 }
